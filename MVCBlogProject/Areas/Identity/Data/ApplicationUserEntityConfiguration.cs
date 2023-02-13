fdc58263688c948054fd876daff51a301497bebb
eliferdals <elif.erdal8818@gop.edu.tr>
2023-02-13 22:37:54 +0300

    Implemented2

--- MVCBlogProject/Areas/Identity/Data/ApplicationUserEntityConfiguration.cs
+++ MVCBlogProject/Areas/Identity/Data/ApplicationUserEntityConfiguration.cs
@@ -3,11 +3,12 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace MVCBlogProject.Areas.Identity.Data
 {
-    internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<object>
+    internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
     {
-        public void Configure(EntityTypeBuilder<object> builder)
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
         {
-            throw new NotImplementedException();
+            builder.Property(u => u.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(u => u.LastName).HasMaxLength(50).IsRequired();
         }
     }
 }
