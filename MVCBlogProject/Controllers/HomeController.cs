77a877f7b77ba24216815620e8fee216790ef4ae
eliferdals <elif.erdal8818@gop.edu.tr>
2023-02-14 00:19:07 +0300

    //Claim based authorization

--- MVCBlogProject/Controllers/HomeController.cs
+++ MVCBlogProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MVCBlogProject.Models;
 using System.Diagnostics;
 
@@ -17,7 +18,7 @@ namespace MVCBlogProject.Controllers
         {
             return View();
         }
-
+        [Authorize(Policy = "IsWriter")] //Claim based authorization
         public IActionResult Privacy()
         {
             return View();
