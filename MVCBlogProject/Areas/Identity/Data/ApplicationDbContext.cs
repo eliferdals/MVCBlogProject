a3986b39567c760a403f851b3939d4d854f98fb3
eliferdals <elif.erdal8818@gop.edu.tr>
2023-02-13 23:11:57 +0300

    created seed data writer roleid with guid method, added role class to id, normalized, pushed database, haspassword

--- MVCBlogProject/Areas/Identity/Data/ApplicationDbContext.cs
+++ MVCBlogProject/Areas/Identity/Data/ApplicationDbContext.cs
@@ -21,7 +21,49 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 
         builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
 
-        //string writerRoleId = Guid.NewGuid().ToString();
-        //string visitorRoleId = Guid.NewGuid().ToString();
+        string writerRoleId = Guid.NewGuid().ToString();
+       // string visitorRoleId = Guid.NewGuid().ToString();
+
+        IdentityRole adminRole = new IdentityRole { Id = writerRoleId, Name = "Writer", NormalizedName = "WRITER" };
+        //IdentityRole standardRole = new IdentityRole { Id = visitorRoleId, Name = "Visitor", NormalizedName = "VISITOR" };
+
+        builder.Entity<IdentityRole>().HasData(writerRoleId);
+       // builder.Entity<IdentityRole>().HasData(visitorRoleId);
+
+        string writerAppUserId = Guid.NewGuid().ToString();
+       // string visitorAppUserId = Guid.NewGuid().ToString();
+
+        var hasher = new PasswordHasher<IdentityUser>();
+
+        ApplicationUser writerUser = new ApplicationUser
+        {
+            Id = writerAppUserId,
+            FirstName = "Writer",
+            LastName = "Writer",
+            Email = "writer@writer.com",
+            NormalizedEmail = "WRITER@WRITER.COM",
+            UserName = "writer@writer.com",
+            NormalizedUserName = "WRITER@WRITER.COM",
+            EmailConfirmed = true
+        };
+
+        writerUser.PasswordHash = hasher.HashPassword(writerUser, "Writer123!");
+
+        //ApplicationUser standardUser = new ApplicationUser
+        //{
+        //    Id = standardAppUserId,
+        //    FirstName = "Standard",
+        //    LastName = "Standard",
+        //    Email = "standard@standard.com",
+        //    NormalizedEmail = "STANDARD@STANDARD.COM",
+        //    UserName = "standard@standard.com",
+        //    NormalizedUserName = "STANDARD@STANDARD.COM",
+        //    EmailConfirmed = true
+        //};
+
+        //standardUser.PasswordHash = hasher.HashPassword(standardUser, "Standard123!");
+
+        builder.Entity<ApplicationUser>().HasData(writerUser);
+        //builder.Entity<ApplicationUser>().HasData(standardUser);
     }
 }
