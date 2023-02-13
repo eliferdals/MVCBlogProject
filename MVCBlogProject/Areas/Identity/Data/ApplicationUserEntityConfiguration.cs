db0e5b328bdb943bac1a4c460a0101466e2938c7
eliferdals <elif.erdal8818@gop.edu.tr>
2023-02-13 22:34:57 +0300

    Implemented Interface

--- MVCBlogProject/Areas/Identity/Data/ApplicationUserEntityConfiguration.cs
+++ MVCBlogProject/Areas/Identity/Data/ApplicationUserEntityConfiguration.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace MVCBlogProject.Areas.Identity.Data
 {
     internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<object>
     {
+        public void Configure(EntityTypeBuilder<object> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
