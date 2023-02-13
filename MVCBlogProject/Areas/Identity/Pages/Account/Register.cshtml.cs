75ff9d38f546437074a4ed1d08aa686ff89120c9
eliferdals <elif.erdal8818@gop.edu.tr>
2023-02-13 23:47:35 +0300

    createuser

--- MVCBlogProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MVCBlogProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -123,6 +123,8 @@ namespace MVCBlogProject.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                user.FirstName = Input.FirstName; 
+                user.LastName = Input.LastName;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
