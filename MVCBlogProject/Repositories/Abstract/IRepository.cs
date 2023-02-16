5a20f77d19454e4a6c6f68879f94340486fa84c0
eliferdals <elif.erdal8818@gop.edu.tr>
2023-02-16 21:00:45 +0300

    Added new Repositories file + Abstact and Concrete. Created Interface and class

--- MVCBlogProject/Repositories/Abstract/IRepository.cs
+++ MVCBlogProject/Repositories/Abstract/IRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace MVCBlogProject.Repositories.Abstract
+﻿using System.Linq.Expressions;
+
+namespace MVCBlogProject.Repositories.Abstract
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        bool Add(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        IEnumerable<T> GetWhere(Expression<Func<T, bool>> predicate);
+        T SingleOrDefault(Expression<Func<T, bool>> predicate);
     }
 }
--- MVCBlogProject/Repositories/Concrete/GenericRepository.cs
+++ MVCBlogProject/Repositories/Concrete/GenericRepository.cs
@@ -1,6 +1,79 @@
-﻿namespace MVCBlogProject.Repositories.Concrete
+﻿using MVCBlogProject.Areas.Identity.Data;
+using System.Linq.Expressions;
+
+namespace MVCBlogProject.Repositories.Concrete
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
     {
+        private readonly ApplicationDbContext db;
+
+        public GenericRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public bool Add(T entity)
+        {
+            try
+            {
+                db.Set<T>().Add(entity);
+                return db.SaveChanges() > 0;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(T entity)
+        {
+            try
+            {
+                db.Set<T>().Remove(entity);
+                return db.SaveChanges() > 0;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return db.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return db.Set<T>().FirstOrDefault(a => a.Id == id);
+            //return db.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetWhere(Expression<Func<T, bool>> predicate)
+        {
+            return db.Set<T>().Where(predicate);
+        }
+
+        public T SingleOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return db.Set<T>().SingleOrDefault(predicate);
+        }
+
+        public bool Update(T entity)
+        {
+            try
+            {
+                //Update metodu içine gönderilen entity'de id varsa ilgili id'ye sahip entity'yi update eder, id yok ise add gibi çalışır. Bu sebeple tek bir AddOrUpdate metodu da kullanılabilir
+                db.Set<T>().Update(entity);
+                return db.SaveChanges() > 0;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
     }
+}
 }
